882a0711f4ccc8e84aef8dc30b0da9b0178a35ef
Rahul Mahawar <sun.projectsdev@gmail.com>
2026-05-09 17:48:28 +0530

    check tets

--- invoices/views.py
+++ invoices/views.py
@@ -188,7 +188,7 @@ def invoice_pdf(request, pk):
     if not (invoice.created_by == request.user or request.user.is_staff or request.user.is_superuser):
         return HttpResponse("You are not authorized to view this invoice.", status=403)
     html_string = render_to_string('invoices/invoice_pdf.html', {'invoice': invoice, "logo_url": f"{request.scheme}://{request.get_host()}/media/uploads/defaultlogo.png"})
-    html = HTML(string=html_string)
+    html = HTML(string=html_string,base_url=settings.MEDIA_ROOT)
     pdf = html.write_pdf()
     response = HttpResponse(pdf, content_type='application/pdf')
     response['Content-Disposition'] = f'filename=invoice_{invoice.id}.pdf'
