e99b82105162c47bb8f8cede93fa5b169de63755
Rahul Mahawar <68722733+Ardhweb@users.noreply.github.com>
2026-07-04 14:23:25 +0530

    bugfix:400 BadRequest

--- sjinvoc/urls.py
+++ sjinvoc/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf import settings #for MEdia Manging Step4
+from django.conf import settings #for Media Manging Step4
 from django.conf.urls.static import static #For Media Manging and Static step4
 #from django.conf.urls import url
 from django.views.generic import TemplateView
